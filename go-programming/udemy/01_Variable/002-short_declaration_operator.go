282aa1f84172ab6f8d850a74c34afd593c3d41fb
Haribhau Hud <hhud@nvidia.com>
2021-11-15 10:21:19 +0530

    short variable declaration and var keyword

---
+++ go-programming/udemy/01_Variable/002-short_declaration_operator.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 100
+	fmt.Println(x)
+	x = 101
+	fmt.Println(x)
+	x = 100 + 200
+	fmt.Println(x)
+
+}
---
+++ go-programming/udemy/01_Variable/003-var-keyword.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var x = 41
+var z int
+
+// var z int = 0
+
+func main() {
+	y := 42
+	z = 43
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
